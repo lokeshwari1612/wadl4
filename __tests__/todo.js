060fc5e6f196821ef2fc7a339d762da23cd3e8fd
lokeshwari1612 <118675627+lokeshwari1612@users.noreply.github.com>
2022-11-20 21:49:20 +0530

    Create todo.js

---
+++ __tests__/todo.js
@@ -0,0 +1,13 @@
+const todoList = require("../todo");
+let today = new Date().toLocaleDateString("en-CA");
+
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+
+describe("Todolist Testing", () => {
+  beforeAll(() => {
+    add({
+      title: "DAA algorithums",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
